da3f8923ec5612059e619eeb7c96c93246f4a7a4
Devvrat12 <devvrattiwari2005@gmail.com>
2026-07-17 16:31:07 +0530

    TEMPORARY TABLES

---
+++ TEMPORARY TABLES.sql
@@ -0,0 +1,28 @@
+-- Temp Tables
+
+CREATE TEMPORARY TABLE temp_table
+(
+first_name varchar(50),
+last_name varchar(50),
+favorite_movie varchar(100)
+);
+
+SELECT *
+FROM temp_table;
+
+INSERT INTO temp_table
+VALUES('ALEX', 'HARMO', 'TERE_NAAM');
+
+CREATE TEMPORARY TABLE salary_over_50K
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT *
+FROM salary_over_50K;
+
+
+
+
+
+
